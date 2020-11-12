cbbb43264962a284333fe84a1211293060d2381c
rmzNadir <dgzrz99@gmail.com>
2020-11-12 14:16:46 -0600

    :recycle: code refactor + new eslint/prettier config

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const config = require("./utils/config");
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const cors = require("cors");
-const blogsRouter = require("./controllers/blogs");
-const middleware = require("./utils/middleware");
-const logger = require("./utils/logger");
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
+const express = require('express');
+const config = require('./utils/config');
+const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
 
-logger.info("connecting to", config.MONGODB_URI);
+const app = express();
+logger.info('connecting to', config.MONGODB_URI);
 
 mongoose
   .connect(config.MONGODB_URI, {
@@ -18,45 +18,43 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => {
-    logger.info("Connected to MongoDB");
+    logger.info('Connected to MongoDB');
   })
   .catch((err) => {
-    logger.error("Error connecting to MongoDB: ", err.message);
+    logger.error('Error connecting to MongoDB: ', err.message);
   });
 
 app.use(cors());
 app.use(express.json());
-morgan.token("body", function (req) {
-  return JSON.stringify(req.body);
-});
+morgan.token('body', (req) => JSON.stringify(req.body));
 
 app.use(
   morgan(function (tokens, req, res) {
-    if (tokens.method(req, res) === "POST") {
+    if (tokens.method(req, res) === 'POST') {
       return [
         tokens.method(req, res),
         tokens.url(req, res),
         tokens.status(req, res),
-        tokens.res(req, res, "content-length"),
-        "-",
-        tokens["response-time"](req, res),
-        "ms",
+        tokens.res(req, res, 'content-length'),
+        '-',
+        tokens['response-time'](req, res),
+        'ms',
         tokens.body(req, res),
-      ].join(" ");
+      ].join(' ');
     }
     return [
       tokens.method(req, res),
       tokens.url(req, res),
       tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
-    ].join(" ");
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
+    ].join(' ');
   })
 );
 
-app.use("/api/blogs", blogsRouter);
+app.use('/api/blogs', blogsRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,22 +1,22 @@
-const logger = require("./logger");
-const chalk = require("chalk");
+const chalk = require('chalk');
+const logger = require('./logger');
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
+  res.status(404).send({ error: 'unknown endpoint' });
 };
 
 const errorHandler = (err, req, res, next) => {
   logger.error(chalk.red(err.message));
 
-  if (err.name === "CastError") {
-    return res.status(400).send({ error: "malformatted id" });
-  } else if (err.name === "ValidationError") {
+  if (err.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  }
+  if (err.name === 'ValidationError') {
     return res
       .status(400)
-      .json({ error: `${err.name}: ${err.message}`, errorName: err.name }); //we send the error message as well as the error name so we can differentiate between multiple types of errors
+      .json({ error: `${err.name}: ${err.message}`, errorName: err.name }); // we send the error message as well as the error name so we can differentiate between multiple types of errors
   }
-
-  next(err);
+  return next(err);
 };
 
 module.exports = {
