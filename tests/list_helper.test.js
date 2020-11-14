763540fed26ee487df43a05ec2a8d9746d1817e1
rmzNadir <dgzrz99@gmail.com>
2020-11-13 23:16:30 -0600

    Exercises 4.6 - 4.7

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -7,6 +7,48 @@ test('dummy returns one', () => {
   expect(result).toBe(1);
 });
 
+const listWithMultipleBlogs = [
+  {
+    title: "Understand JavaScript's this Keyword in Depth",
+    author: 'Marius Schulz',
+    url:
+      'https://egghead.io/courses/understand-javascript-s-this-keyword-in-depth',
+    likes: 2675,
+    id: '5f6fdf9b5532ad0afe03ef16',
+  },
+  {
+    title: 'Inversion of Control',
+    author: 'Kent C. Dodds',
+    url: 'https://kentcdodds.com/blog/inversion-of-control',
+    likes: 2077,
+    id: '5f6fe4a4e57bc010123a0508',
+  },
+  {
+    title: 'Windows XP source code leaks online',
+    author: 'Tom Warren',
+    url:
+      'https://www.theverge.com/2020/9/25/21455655/microsoft-windows-xp-source-code-leak',
+    likes: 69420,
+    id: '5f6fe530e57bc010123a0509',
+  },
+  {
+    title: 'Windows XP source code leaks online',
+    author: 'Tom Warren',
+    url:
+      'https://www.theverge.com/2020/9/25/21455655/microsoft-windows-xp-source-code-leak',
+    likes: 120,
+    id: '5f6fe530e57bc010123a0501',
+  },
+  {
+    title: 'Windows XP source code leaks online',
+    author: 'Tom Warren',
+    url:
+      'https://www.theverge.com/2020/9/25/21455655/microsoft-windows-xp-source-code-leak',
+    likes: 1,
+    id: '5f6fe530e57bc010123a0501',
+  },
+];
+
 describe('Total likes', () => {
   const emptyList = [];
 
@@ -21,32 +63,6 @@ describe('Total likes', () => {
     },
   ];
 
-  const listWithMultipleBlogs = [
-    {
-      title: "Understand JavaScript's this Keyword in Depth",
-      author: 'Marius Schulz',
-      url:
-        'https://egghead.io/courses/understand-javascript-s-this-keyword-in-depth',
-      likes: 2675,
-      id: '5f6fdf9b5532ad0afe03ef16',
-    },
-    {
-      title: 'Inversion of Control',
-      author: 'Kent C. Dodds',
-      url: 'https://kentcdodds.com/blog/inversion-of-control',
-      likes: 2077,
-      id: '5f6fe4a4e57bc010123a0508',
-    },
-    {
-      title: 'Windows XP source code leaks online',
-      author: 'Tom Warren',
-      url:
-        'https://www.theverge.com/2020/9/25/21455655/microsoft-windows-xp-source-code-leak',
-      likes: 69420,
-      id: '5f6fe530e57bc010123a0509',
-    },
-  ];
-
   test('Amount of likes on an empty blog list is zero', () => {
     expect(listHelper.totalLikes(emptyList)).toBe(0);
   });
@@ -56,37 +72,11 @@ describe('Total likes', () => {
   });
 
   test('of a bigger list is calculated right', () => {
-    expect(listHelper.totalLikes(listWithMultipleBlogs)).toBe(74172);
+    expect(listHelper.totalLikes(listWithMultipleBlogs)).toBe(74293);
   });
 });
 
 describe('Most liked', () => {
-  const listWithMultipleBlogs = [
-    {
-      title: "Understand JavaScript's this Keyword in Depth",
-      author: 'Marius Schulz',
-      url:
-        'https://egghead.io/courses/understand-javascript-s-this-keyword-in-depth',
-      likes: 2675,
-      id: '5f6fdf9b5532ad0afe03ef16',
-    },
-    {
-      title: 'Inversion of Control',
-      author: 'Kent C. Dodds',
-      url: 'https://kentcdodds.com/blog/inversion-of-control',
-      likes: 2077,
-      id: '5f6fe4a4e57bc010123a0508',
-    },
-    {
-      title: 'Windows XP source code leaks online',
-      author: 'Tom Warren',
-      url:
-        'https://www.theverge.com/2020/9/25/21455655/microsoft-windows-xp-source-code-leak',
-      likes: 69420,
-      id: '5f6fe530e57bc010123a0509',
-    },
-  ];
-
   const mostLikedBlog = {
     title: 'Windows XP source code leaks online',
     author: 'Tom Warren',
@@ -100,5 +90,23 @@ describe('Most liked', () => {
     expect(listHelper.favoriteBlog(listWithMultipleBlogs)).toEqual(
       mostLikedBlog
     );
+  });
+});
+
+describe('Author with most blogs', () => {
+  test('from a pool of various blogs is calculated right', () => {
+    expect(listHelper.mostBlogs(listWithMultipleBlogs)).toEqual({
+      author: 'Tom Warren',
+      blogs: 3,
+    });
+  });
+});
+
+describe('Author with most likes', () => {
+  test('from a pool of various blogs is calculated right', () => {
+    expect(listHelper.mostLikes(listWithMultipleBlogs)).toEqual({
+      author: 'Tom Warren',
+      likes: 69541,
+    });
   });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash');
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -12,8 +14,40 @@ const favoriteBlog = (blogs) => {
   return blogs.filter((blog) => blog.likes === mostLiked)[0];
 };
 
+const mostBlogs = (blogs) => {
+  const blogsPerAuthor = _.countBy(blogs, 'author');
+  const author = _.max(
+    Object.keys(blogsPerAuthor),
+    (Author) => blogsPerAuthor[Author]
+  );
+  // console.log('blogsPerAuthor', blogsPerAuthor);
+  // console.log('Author', Author);
+  const obj = {
+    author: author,
+    blogs: blogsPerAuthor[author],
+  };
+  return obj;
+};
+
+const mostLikes = (blogs) => {
+  const likesPerAuthor = _(blogs)
+    .groupBy('author')
+    .map((objs, key) => ({
+      author: key,
+      likes: _.sumBy(objs, 'likes'),
+    }))
+    .value();
+
+  const author = _.maxBy(likesPerAuthor, (object) => object.likes);
+  // console.log('likesPerAuthor', likesPerAuthor);
+  // console.log('author', author);
+  return { ...author };
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
