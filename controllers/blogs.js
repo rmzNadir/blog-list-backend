1307ed40f430538bdf67f45258678f4cbe9bc96f
rmzNadir <dgzrz99@gmail.com>
2020-11-23 23:14:25 -0600

    updated creation and modification of a blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -33,32 +33,38 @@ blogsRouter.post('/', async (req, res) => {
 
   const { title, author, url, likes } = req.body;
 
-  const blog = new Blog({
+  const newBlog = new Blog({
     title,
     author,
     url,
     likes: likes === undefined ? 0 : likes,
     user: user._id,
-  });
+  }).populate('user', { blogsPosted: 0 });
 
-  const savedBlog = await blog.save();
+  const savedBlog = await newBlog.save();
   user.blogsPosted = user.blogsPosted.concat(savedBlog._id);
   await user.save();
 
-  return res.json(savedBlog);
+  const blog = await Blog.findById(savedBlog.id).populate('user', {
+    blogsPosted: 0,
+  });
+
+  return res.json(blog);
 });
 
 blogsRouter.patch('/:id', async (req, res) => {
   const { id } = req.params;
+
   const data = await Blog.findByIdAndUpdate(
     { _id: id },
     { $set: req.body },
     { new: true }
-  );
-  res.json({
+  ).populate('user', { blogsPosted: 0 });
+  return res.json({
     status: 200,
-    msg: 'Blog updated',
-    data: data,
+    success: true,
+    msg: 'blog updated',
+    data,
   });
 });
 
